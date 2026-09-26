45733e619e2de957be4cbc61e0afc163e65a846c
Mariia Katsala <ubuntovka@gmail.com>
2026-09-26 23:00:59 +0200

    Add about page

---
+++ app/about/page.tsx
@@ -0,0 +1,12 @@
+import styles from "../page.module.css";
+
+export default function AboutPage() {
+  return (
+    <main className={styles.page}>
+      <section>
+        <h1>About</h1>
+        <p>Northstar and Leafline help teams plan projects and grow their skills.</p>
+      </section>
+    </main>
+  );
+}
